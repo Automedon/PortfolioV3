656840bb0afa5e81ce41be835c798f430063a18f
Automedon <42299107+Automedon@users.noreply.github.com>
2021-11-17 13:32:42 +0700

    Scrollbar

--- src/styles/globals.js
+++ src/styles/globals.js
@@ -1,5 +1,5 @@
-import { createGlobalStyle } from 'styled-components';
-import { normalize } from 'styled-normalize';
+import { createGlobalStyle } from "styled-components";
+import { normalize } from "styled-normalize";
 
 const GlobalStyles = createGlobalStyle`
   ${normalize};
@@ -15,15 +15,15 @@ const GlobalStyles = createGlobalStyle`
 
   }
   body {
-    font-family: ${props => props.theme.fonts.main};
+    font-family: ${(props) => props.theme.fonts.main};
     font-size: 1.6rem;
-    background: ${props => props.theme.colors.background1};
-    color: ${props => props.theme.colors.primary1};
+    background: ${(props) => props.theme.colors.background1};
+    color: ${(props) => props.theme.colors.primary1};
     cursor: default;
 
   }
   h1,h2,h3,h4,h5,h6,button {
-    font-family: ${props => props.theme.fonts.title};
+    font-family: ${(props) => props.theme.fonts.title};
   }
   a {
     text-decoration: none;
@@ -31,7 +31,21 @@ const GlobalStyles = createGlobalStyle`
   li{
     list-style: none;
   }
-
+  ::-webkit-scrollbar{
+    width:15px;
+  }
+  ::-webkit-scrollbar-track{
+    background: #d4d4d4;
+    box-shadow: 0 0 2px rgba(0,0,0,.2) inset;
+  }
+  ::-webkit-scrollbar-thumb{
+    background: #939393;
+    border-radius: 8px;
+    border:3px solid #d4d4d4;
+  }
+  ::-webkit-scrollbar-thumb:hover{
+    background: #7c7c7c;
+  }
 `;
 
 export default GlobalStyles;
